9bedc1cc851f596c455a8f29ce74e2d8e7cfc634
Ruidy <nemausatpro@gmail.com>
2021-09-16 16:35:04 +0200

    examples

---
+++ go/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
+	})
+	router.Run(":80") // http://0.0.0.0/
+}
