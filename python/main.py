0efdda90de52d567fcc4b5c7297067cb97a6e955
Ruidy <nemausatpro@gmail.com>
2021-09-23 21:27:09 +0200

    up

--- python/main.py
+++ python/main.py
@@ -6,10 +6,10 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+def read_root() -> dict:
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
+def read_item(item_id: int, q: Optional[str] = None) -> dict:
     return {"item_id": item_id, "q": q}
